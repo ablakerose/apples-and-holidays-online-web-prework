4489f8ce3c9e022298cd6d0eb73c62857e2409a5
Blake Rose <bdecktor@gmail.com>
2019-05-13 01:08:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,11 +17,7 @@ holiday_supplies = {
 def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
-
 second_supply_for_fourth_of_july(holiday_supplies)
-
-
-# Learn.co Textbook solution
 def add_supply_to_winter_holidays(holiday_hash, item)
  holiday_hash[:winter].each do |holiday, decorations|
    decorations << item
